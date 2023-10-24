b8d2c1ef3fb64dff963ca81387c15c789fb6bad6
simonleandergrimm <simonleandergrimm@gmail.com>
2023-10-24 11:36:42 -0400

    Changed determine_hv_taxids to only consider kraken assignments as hv taxids,not mere matches.

--- validation/determine_hv_taxids.py
+++ validation/determine_hv_taxids.py
@@ -16,11 +16,9 @@ with open(human_viruses) as inf:
 
 for fname in glob.glob("%s/*.json" % hvreads_dir):
     with open(fname) as inf:
-        for kraken_info, *_ in json.load(inf).values():
-            for token in kraken_info.split():
-                taxid, count = token.split(":")
-                if taxid in hv_taxids:
-                    seen_hv_taxids.add(int(taxid))
+        for kraken_assignment, _, *_ in json.load(inf).values():
+            if kraken_assignment in hv_taxids:
+                seen_hv_taxids.add(int(taxid))
 
 with open(taxids_out, "w") as outf:
     for taxid in sorted(seen_hv_taxids):
