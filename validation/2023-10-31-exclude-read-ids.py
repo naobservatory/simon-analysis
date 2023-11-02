5b57384f12fa6d9d6559ab48202970bce21d2ac0
simonleandergrimm <simonleandergrimm@gmail.com>
2023-11-02 11:09:58 -0400

    newest exclusion script, with log length adjusted alignment score < 20.

--- validation/2023-10-31-exclude-read-ids.py
+++ validation/2023-10-31-exclude-read-ids.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
-from return_hv_taxid_dict import hv_taxid_dict
 from return_sam_records import sam_records
 import pandas as pd
 
 
 def start():
-    hv_records = hv_taxid_dict()
     df = sam_records()
 
     dropped_merged = df[
-        (df["read_type"] == "combined") & (df["length_adj_score"] < 10)
+        (df["read_type"] == "combined") & (df["log_length_adj_score"] < 20)
     ]["read_id"].tolist()
 
     dropped_pair_low_score = (
         df[df["read_type"].str.startswith("read_")]
         .groupby("read_id")
-        .filter(lambda x: (x["length_adj_score"].max() < 10))["read_id"]
+        .filter(lambda x: (x["log_length_adj_score"].max() < 20))["read_id"]
         .tolist()
     )
 
     excluded_reads_set = set(dropped_merged + dropped_pair_low_score)
 
-    with open("excluded_read_ids.txt", "w") as outf:
+    with open("excluded-read-ids.txt", "w") as outf:
         for read in excluded_reads_set:
             outf.write(f"{read}\n")
 
